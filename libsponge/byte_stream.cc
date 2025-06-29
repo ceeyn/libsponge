5d9fa8efe7e833fc1f296ac6700265871db9e5d4
ceeyn <ceeyn@github.com>
2025-06-29 23:35:57 +0800

    lab0

--- libsponge/byte_stream.cc
+++ libsponge/byte_stream.cc
@@ -12,42 +12,71 @@ void DUMMY_CODE(Targs &&... /* unused */) {}
 
 using namespace std;
 
-ByteStream::ByteStream(const size_t capacity) { DUMMY_CODE(capacity); }
+ByteStream::ByteStream(const size_t capacity)
+    : _capacity(capacity),
+     _buffer(),           // ✅ 先初始化 _buffer
+     _bytes_written(0),
+     _bytes_read(0),
+     _input_ended(false), // ✅ 再初始化 _input_ended
+     _error(false) {}
+
 
 size_t ByteStream::write(const string &data) {
-    DUMMY_CODE(data);
-    return {};
+    if (_input_ended || remaining_capacity() == 0) return 0;
+    size_t write_len = min(data.size(), remaining_capacity());
+    // 批量插入：O(1) 时间复杂度
+    _buffer.insert(_buffer.end(), data.begin(), data.begin() + write_len);
+    _bytes_written += write_len;
+    return write_len;
 }
 
 //! \param[in] len bytes will be copied from the output side of the buffer
 string ByteStream::peek_output(const size_t len) const {
-    DUMMY_CODE(len);
-    return {};
+    // DUMMY_CODE(len);
+    return std::string(_buffer.begin(), _buffer.begin() + std::min(len, _buffer.size()));
 }
 
 //! \param[in] len bytes will be removed from the output side of the buffer
-void ByteStream::pop_output(const size_t len) { DUMMY_CODE(len); }
-
-//! Read (i.e., copy and then pop) the next "len" bytes of the stream
-//! \param[in] len bytes will be popped and returned
-//! \returns a string
-std::string ByteStream::read(const size_t len) {
-    DUMMY_CODE(len);
-    return {};
+void ByteStream::pop_output(const size_t len) {
+    size_t remove_count = min(len, _buffer.size());
+    _buffer.erase(_buffer.begin(), _buffer.begin() + remove_count); // O(1)删除
+    _bytes_read += remove_count;
 }
 
-void ByteStream::end_input() {}
+std::string ByteStream::read(const size_t len) {
+    std::string result = peek_output(len);
+    pop_output(result.size());
+    return result;
+}
 
-bool ByteStream::input_ended() const { return {}; }
+void ByteStream::end_input() {
+    _input_ended = true;
+}
 
-size_t ByteStream::buffer_size() const { return {}; }
+bool ByteStream::input_ended() const {
+    return _input_ended;
+}
 
-bool ByteStream::buffer_empty() const { return {}; }
+size_t ByteStream::buffer_size() const { 
+    return _buffer.size(); 
+}
 
-bool ByteStream::eof() const { return false; }
+bool ByteStream::buffer_empty() const { 
+    return _buffer.empty();
+}
 
-size_t ByteStream::bytes_written() const { return {}; }
+bool ByteStream::eof() const { 
+    return input_ended() && buffer_empty(); 
+}
 
-size_t ByteStream::bytes_read() const { return {}; }
+size_t ByteStream::bytes_written() const { 
+    return _bytes_written; 
+}
 
-size_t ByteStream::remaining_capacity() const { return {}; }
+size_t ByteStream::bytes_read() const { 
+    return _bytes_read; 
+}
+
+size_t ByteStream::remaining_capacity() const { 
+    return _capacity - buffer_size();
+}
--- libsponge/byte_stream.hh
+++ libsponge/byte_stream.hh
@@ -2,7 +2,7 @@
 #define SPONGE_LIBSPONGE_BYTE_STREAM_HH
 
 #include <string>
-
+#include <deque>
 //! \brief An in-order byte stream.
 
 //! Bytes are written on the "input" side and read from the "output"
@@ -16,13 +16,22 @@ class ByteStream {
     // all, but if any of your tests are taking longer than a second,
     // that's a sign that you probably want to keep exploring
     // different approaches.
-
+    // std::deque<char> _buffer;
+    // size_t _capacity;
+    // bool _input_ended = false;  // 是否结束写入
+    // size_t _bytes_written = 0;
+    // size_t _bytes_read = 0;
+    size_t _capacity;
+    std::deque<char> _buffer;
+    size_t _bytes_written;
+    size_t _bytes_read;
+    bool _input_ended;
     bool _error{};  //!< Flag indicating that the stream suffered an error.
 
   public:
     //! Construct a stream with room for `capacity` bytes.
     ByteStream(const size_t capacity);
-
+    
     //! \name "Input" interface for the writer
     //!@{
 
@@ -80,6 +89,6 @@ class ByteStream {
     //! Total number of bytes popped
     size_t bytes_read() const;
     //!@}
-};
+  };
 
 #endif  // SPONGE_LIBSPONGE_BYTE_STREAM_HH
