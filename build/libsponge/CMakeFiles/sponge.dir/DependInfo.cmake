c52859974c806fa04814b743fa8f49ca4a70d01d
ceeyn <ceeyn@github.com>
2025-11-07 00:13:52 +0000

    first

---
+++ build/libsponge/CMakeFiles/sponge.dir/DependInfo.cmake
@@ -0,0 +1,37 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/sponge/libsponge/byte_stream.cc" "/sponge/build/libsponge/CMakeFiles/sponge.dir/byte_stream.cc.o"
+  "/sponge/libsponge/stream_reassembler.cc" "/sponge/build/libsponge/CMakeFiles/sponge.dir/stream_reassembler.cc.o"
+  "/sponge/libsponge/tcp_helpers/tcp_header.cc" "/sponge/build/libsponge/CMakeFiles/sponge.dir/tcp_helpers/tcp_header.cc.o"
+  "/sponge/libsponge/tcp_helpers/tcp_segment.cc" "/sponge/build/libsponge/CMakeFiles/sponge.dir/tcp_helpers/tcp_segment.cc.o"
+  "/sponge/libsponge/tcp_helpers/tcp_state.cc" "/sponge/build/libsponge/CMakeFiles/sponge.dir/tcp_helpers/tcp_state.cc.o"
+  "/sponge/libsponge/tcp_receiver.cc" "/sponge/build/libsponge/CMakeFiles/sponge.dir/tcp_receiver.cc.o"
+  "/sponge/libsponge/util/address.cc" "/sponge/build/libsponge/CMakeFiles/sponge.dir/util/address.cc.o"
+  "/sponge/libsponge/util/buffer.cc" "/sponge/build/libsponge/CMakeFiles/sponge.dir/util/buffer.cc.o"
+  "/sponge/libsponge/util/eventloop.cc" "/sponge/build/libsponge/CMakeFiles/sponge.dir/util/eventloop.cc.o"
+  "/sponge/libsponge/util/file_descriptor.cc" "/sponge/build/libsponge/CMakeFiles/sponge.dir/util/file_descriptor.cc.o"
+  "/sponge/libsponge/util/parser.cc" "/sponge/build/libsponge/CMakeFiles/sponge.dir/util/parser.cc.o"
+  "/sponge/libsponge/util/socket.cc" "/sponge/build/libsponge/CMakeFiles/sponge.dir/util/socket.cc.o"
+  "/sponge/libsponge/util/tun.cc" "/sponge/build/libsponge/CMakeFiles/sponge.dir/util/tun.cc.o"
+  "/sponge/libsponge/util/util.cc" "/sponge/build/libsponge/CMakeFiles/sponge.dir/util/util.cc.o"
+  "/sponge/libsponge/wrapping_integers.cc" "/sponge/build/libsponge/CMakeFiles/sponge.dir/wrapping_integers.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../libsponge/util"
+  "../libsponge/tcp_helpers"
+  "../libsponge"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
--- libsponge/byte_stream.cc
+++ libsponge/byte_stream.cc
@@ -1,53 +1,71 @@
 #include "byte_stream.hh"
-
-// Dummy implementation of a flow-controlled in-memory byte stream.
-
-// For Lab 0, please replace with a real implementation that passes the
-// automated checks run by `make check_lab0`.
-
-// You will need to add private members to the class declaration in `byte_stream.hh`
+#include <algorithm>  // std::min
 
 template <typename... Targs>
 void DUMMY_CODE(Targs &&... /* unused */) {}
 
 using namespace std;
 
-ByteStream::ByteStream(const size_t capacity) { DUMMY_CODE(capacity); }
-
-size_t ByteStream::write(const string &data) {
-    DUMMY_CODE(data);
-    return {};
+ByteStream::ByteStream(const size_t capacity)
+    : _capacity(capacity),
+      _buffer(),
+      _written_size(0),
+      _read_size(0),
+      _is_ended(false),
+      _error(false) {}
+// ... existing code ...
+size_t ByteStream::write(const std::string &data) {
+    if (_is_ended || remaining_capacity() == 0) return 0;
+    size_t writing_size = std::min(data.size(), remaining_capacity());
+    _buffer.insert(_buffer.end(), data.begin(), data.begin() + writing_size);
+    _written_size += writing_size;
+    return writing_size;
 }
-
-//! \param[in] len bytes will be copied from the output side of the buffer
+// ... existing code ...
 string ByteStream::peek_output(const size_t len) const {
-    DUMMY_CODE(len);
-    return {};
+    return std::string(_buffer.begin(), _buffer.begin() + std::min(len, _buffer.size()));
 }
-
-//! \param[in] len bytes will be removed from the output side of the buffer
-void ByteStream::pop_output(const size_t len) { DUMMY_CODE(len); }
-
-//! Read (i.e., copy and then pop) the next "len" bytes of the stream
-//! \param[in] len bytes will be popped and returned
-//! \returns a string
+// ... existing code ...
+void ByteStream::pop_output(const size_t len) {
+    size_t remove_count = std::min(len, _buffer.size());
+    _buffer.erase(_buffer.begin(), _buffer.begin() + remove_count);
+    _read_size += remove_count;
+}
+// ... existing code ...
 std::string ByteStream::read(const size_t len) {
-    DUMMY_CODE(len);
-    return {};
+    std::string result = peek_output(len);
+    pop_output(result.size());
+    return result;
 }
-
-void ByteStream::end_input() {}
-
-bool ByteStream::input_ended() const { return {}; }
-
-size_t ByteStream::buffer_size() const { return {}; }
-
-bool ByteStream::buffer_empty() const { return {}; }
-
-bool ByteStream::eof() const { return false; }
-
-size_t ByteStream::bytes_written() const { return {}; }
-
-size_t ByteStream::bytes_read() const { return {}; }
-
-size_t ByteStream::remaining_capacity() const { return {}; }
+// ... existing code ...
+void ByteStream::end_input() {
+    _is_ended = true;
+}
+// ... existing code ...
+bool ByteStream::input_ended() const {
+    return _is_ended;
+}
+// ... existing code ...
+size_t ByteStream::buffer_size() const {
+    return _buffer.size();
+}
+// ... existing code ...
+bool ByteStream::buffer_empty() const {
+    return _buffer.empty();
+}
+// ... existing code ...
+bool ByteStream::eof() const {
+    return input_ended() && buffer_empty();
+}
+// ... existing code ...
+size_t ByteStream::bytes_written() const {
+    return _written_size;
+}
+// ... existing code ...
+size_t ByteStream::bytes_read() const {
+    return _read_size;
+}
+// ... existing code ...
+size_t ByteStream::remaining_capacity() const {
+    return _capacity - _buffer.size();
+}
